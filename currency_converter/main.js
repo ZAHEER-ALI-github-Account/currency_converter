d9bf1b0276994c4e84cc65906e8c51575633d33b
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-20 16:05:56 +0500

    Currency_Converter

---
+++ currency_converter/main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let c_value = {
+    PKR: 278.40,
+    UAE: 3.67,
+    USD: 1,
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "Converting from: ",
+        choices: ['PKR', 'UAE', 'USD']
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "Converting to: ",
+        choices: ['PKR', 'UAE', 'USD']
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Your amount to convert: "
+    }]);
+console.log(c_value[ans.to] / c_value[ans.from] * ans.amount);
